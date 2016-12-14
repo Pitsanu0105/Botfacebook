b6af0c9c543aebce896a1b8bbd7ff3f446efbbba
Pitsanu0105 <jeng.39@hotmail.com>
2016-12-14 23:22:01 +0700

    Update server.js

--- server.js
+++ server.js
@@ -201,12 +201,17 @@
         //------------------กำหนดการ---------------------------------------------------
         function Programs(recipientId, messageText) {
             var it3kquerry = data3k.find(data => data.type === 'Program')
+            var messageData = {
+                recipient: {
+                    id: recipientId
+                }
             console.log(it3kquerry);
               console.log(it3kquerry.location);
               console.log(it3kquerry.time);
               console.log(it3kquerry.message);
               sendTextMessage(senderID, "สถานที่");
             callSendAPI(messageData);
+          }
         }
         //-----------------------------------------------------------------------------
         //----------------ตอบกลับ------------------------------------------------------
