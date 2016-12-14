503ea010a52dd0df5792d75a705bd6c199ccccb9
Pitsanu0105 <jeng.39@hotmail.com>
2016-12-14 23:18:53 +0700

    Update server.js

--- server.js
+++ server.js
@@ -201,6 +201,10 @@
         //------------------กำหนดการ---------------------------------------------------
         function Programs(recipientId, messageText) {
             var it3kquerry = data3k.find(data => data.type === 'Program')
+            var messageData = {
+                recipient: {
+                    id: recipientId
+                }
             console.log(it3kquerry);
               console.log(it3kquerry.location);
               console.log(it3kquerry.time);
