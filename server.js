7f79282665f6e4c74026143f47f6b5fd507c2c20
Pitsanu0105 <jeng.39@hotmail.com>
2016-12-15 11:45:41 +0700

    Update server.js

--- server.js
+++ server.js
@@ -206,6 +206,7 @@
                 console.log(it3kquerry.time);
                 console.log(it3kquerry.message);
                 sendTextMessage(recipientId," it3kquerry.location")
+                sendGreetMessage(senderID)
 
 
             /*  var messageData = {
