e67122a2b759a1b8be84ecfbbef9c09ae0ffd401
salapao2136 <potook_02@hotmail.com>
2016-12-15 15:20:54 +0700

    Update server.js

--- server.js
+++ server.js
@@ -194,22 +194,14 @@ function Programs (recipientId) {
         type: 'template',
         payload: {
           template_type: 'generic',
-          elements: [
-            {
-              title: it3kquerry.message,
-              image_url: 'https://lh3.googleusercontent.com/MOf9Kxxkj7GvyZlTZOnUzuYv0JAweEhlxJX6gslQvbvlhLK5_bSTK6duxY2xfbBsj43H=w300',
-              buttons: [{
-                type: 'postback',
-                title: 'รายละเอียด',
-                payload: 'detail'
-              }]
-            }
-          ]
+          elements: []
         }
       }
       // text: `สถานที่คือ ${it3kquerry.location} เวลาคือ ${it3kquerry.time} ข้อความ ${it3kquerry.message}`
     }
   }
+  let pic = 'https://lh3.googleusercontent.com/MOf9Kxxkj7GvyZlTZOnUzuYv0JAweEhlxJX6gslQvbvlhLK5_bSTK6duxY2xfbBsj43H=w300'
+  it3kquerry.forEach((item) => { messageData.message.attachment.payload.elements.push({title: item.message, image_url: pic, buttons: [{type: 'postback', title: 'รายละเอียด', payload: 'detail'}]}) })
   console.log('==============================Program==========================')
 
   callSendAPI(messageData)
