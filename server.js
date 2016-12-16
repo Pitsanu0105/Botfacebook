b9c73f75e38284c6235dae9e98ade07b0d833322
Pitsanu0105 <jeng.39@hotmail.com>
2016-12-16 14:35:51 +0700

    Update server.js

--- server.js
+++ server.js
@@ -342,7 +342,7 @@ function sendGreetMessage (recipientId, messageText) {
 }
 // ------------ผลการเเข่งขัน---------------//
 function Result (recipientId, messageText) {
-  var it3kquerrysport = data3k.filter(data => data.type === 'sport')
+  var it3kquerrysport = data3k.filter(data => data.type === 'sport' && data.status === 'จบการเเข่งขัน')
   //it3kquerrysport.forEach()
   console.log('bbbbbb',it3kquerrysport);
   console.log('aaaaaaaaaa',it3kquerrysport[0].status);
